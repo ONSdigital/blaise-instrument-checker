8de954ef27109e64d998af7a0f6786d3e637ccf9
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-10-26 14:58:12 +0000

    Status's improvement

--- blaise_instrument_checker/api.py
+++ blaise_instrument_checker/api.py
@@ -23,24 +23,27 @@ def health_check():
 
 @app.route('/api/instruments', methods=["GET"])
 def get_all_instruments_on_blaise():
-    host = request.args.get('vm_name', None, type=str)
+    BLAISE_HOST = request.args.get('vm_name', None, type=str)
 
-    app.logger.info(f"Host : {host}")
-
+    app.logger.info(f"BLAISE_HOST : {BLAISE_HOST}")
     app.logger.info(f"PROTOCOL : {PROTOCOL}")
     app.logger.info(f"BLAISE_USERNAME : {BLAISE_USERNAME}")
 
     try:
-        status, token = pyblaise.get_auth_token(PROTOCOL, host, 8031, BLAISE_USERNAME, BLAISE_PASSWORD)
+        status, token = pyblaise.get_auth_token(PROTOCOL, BLAISE_HOST, 8031, BLAISE_USERNAME, BLAISE_PASSWORD)
         app.logger.debug(f"get_auth_token Status: {status}")
+        if status != 200:
+            return jsonify({"status": "error", "message": f"Could not get authentication token from blaise on '{PROTOCOL}://{BLAISE_HOST}' as '{BLAISE_USERNAME}'"}), 401
     except Exception as e:
-        app.logger.exception(f"could not get authentication token from blaise on '{PROTOCOL}://{host}' as '{BLAISE_USERNAME}'")
-        return jsonify("false"), 500
+        app.logger.exception(f"Could not get authentication token from blaise on '{PROTOCOL}://{BLAISE_HOST}' as '{BLAISE_USERNAME}'")
+        return jsonify({"status": "error", "message": f"Could not get authentication token from blaise on '{PROTOCOL}://{BLAISE_HOST}' as '{BLAISE_USERNAME}'"}), 401
 
     try:
-        status, instruments = pyblaise.get_list_of_instruments(PROTOCOL, host, 8031, token)
+        status, instruments = pyblaise.get_list_of_instruments(PROTOCOL, BLAISE_HOST, 8031, token)
         app.logger.info(f"get_list_of_instruments status: {status}")
+        if status != 200:
+            return jsonify({"status": "error", "message": f"get list of instruments on '{PROTOCOL}://{BLAISE_HOST}' as '{BLAISE_USERNAME}' failed"}), status
         return jsonify(instruments), 200
     except Exception as e:
-        app.logger.exception(f"could not get list of instruments from blaise on '{PROTOCOL}://{host}' as '{BLAISE_USERNAME}'")
-        return jsonify("false"), 500
+        app.logger.exception(f"get list of instruments on '{PROTOCOL}://{BLAISE_HOST}' as '{BLAISE_USERNAME}' failed'")
+        return jsonify({"status": "error", "message": f"get list of instruments on '{PROTOCOL}://{BLAISE_HOST}' as '{BLAISE_USERNAME}' failed"}), status
